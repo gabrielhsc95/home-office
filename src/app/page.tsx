8a19399ba105f43a7d955da3c2e0ffa7aaa40ecb
Gabriel Cardoso <gabrielhsc95@gmail.com>
2025-04-26 00:42:12 +0000

    good, but I would like glow when not selected, very faded, but still visible that it is an interactive area

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -48,19 +48,19 @@ export default function Home() {
           />
           {/* Hover areas */}
           <div
-            className="absolute top-1/4 left-1/4 w-40 h-20 cursor-pointer hover:ring-2 hover:ring-teal-500 hover:ring-offset-2 transition-shadow duration-200"
+            className="absolute top-1/4 left-1/4 w-40 h-20 cursor-pointer hover:ring-2 hover:ring-teal-500 hover:ring-offset-2 transition-shadow duration-200 ring-1 ring-teal-500/20"
             onMouseEnter={() => handleEquipmentHover('monitor')}
           />
           <div
-            className="absolute bottom-1/4 left-1/4 w-40 h-20 cursor-pointer hover:ring-2 hover:ring-teal-500 hover:ring-offset-2 transition-shadow duration-200"
+            className="absolute bottom-1/4 left-1/4 w-40 h-20 cursor-pointer hover:ring-2 hover:ring-teal-500 hover:ring-offset-2 transition-shadow duration-200 ring-1 ring-teal-500/20"
             onMouseEnter={() => handleEquipmentHover('keyboard')}
           />
           <div
-            className="absolute top-1/2 right-1/4 w-40 h-20 cursor-pointer hover:ring-2 hover:ring-teal-500 hover:ring-offset-2 transition-shadow duration-200"
+            className="absolute top-1/2 right-1/4 w-40 h-20 cursor-pointer hover:ring-2 hover:ring-teal-500 hover:ring-offset-2 transition-shadow duration-200 ring-1 ring-teal-500/20"
             onMouseEnter={() => handleEquipmentHover('mouse')}
           />
           <div
-            className="absolute bottom-1/4 right-1/4 w-40 h-20 cursor-pointer hover:ring-2 hover:ring-teal-500 hover:ring-offset-2 transition-shadow duration-200"
+            className="absolute bottom-1/4 right-1/4 w-40 h-20 cursor-pointer hover:ring-2 hover:ring-teal-500 hover:ring-offset-2 transition-shadow duration-200 ring-1 ring-teal-500/20"
             onMouseEnter={() => handleEquipmentHover('speakers')}
           />
         </div>
